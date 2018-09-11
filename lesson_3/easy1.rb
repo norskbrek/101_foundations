f423bbf50d4b52a06c0c5156126b08fa812e5734
norskbrek <norskbrek@github.com>
2018-09-11 13:45:13 -0500

    add lesson 3

---
+++ lesson_3/easy1.rb
@@ -0,0 +1,45 @@
+=begin
+
+Question 1.
+Expect the code to print 1, 2, 2, 3.
+Despite using the #uniq method, we don't use it destructively.
+
+Question 2.
+! is used to: denote a destructive method, and to denote
+that something is "not" - "not"-equal, "not"-true, etc.
+? denotes a questioning method (#include?)
+
+1 - != means "not equal to" - used for booleans and comparisons.
+2 - Turns the boolean value to its opposite.
+3 - The method is destructive (mutates caller).
+4 - Ternary operator.
+5 - Iterators over something to see if it evaluates to true.
+6 - Turns object into boolean equivalent.
+
+Question 3.
+advice.gsub!("important", "urgent")
+
+Question 4.
+#delete_at() Deletes value at specified index number.
+#delete() Deletes specified value (regardless of index).
+
+Question 5.
+(10..100).cover?(42)
+
+Question 6.
+famous_words = "seven years ago..."
+before_famous_words = "four score and "
+before_famous_words << famous_words
+
+famous_words = "seven years ago..."
+x = "four score and "
+puts x + famous_words
+
+Question 7.
+42
+
+Question 8.
+flintstones.flatten!
+
+Question 9.
+flintstones.assoc("Barney")
