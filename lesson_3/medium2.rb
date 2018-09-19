753a0cf68ec537f26bb8c188617c1816c8ba831b
norskbrek <norskbrek@github.com>
2018-09-19 16:51:51 -0500

    add medium2 exercises

---
+++ lesson_3/medium2.rb
@@ -0,0 +1,22 @@
+=begin
+
+Question 1.
+To be continued...
+
+Question 2.
+To be continued...
+
+Question 3.
+my_string = "pumpkins" - not mutated by method (just re-assignment).
+my_array = ["pumpkins", "rutabaga"] - mutated by method.
+
+Question 4.
+my_string = "pumpkinsrutabaga"
+my_array = ["pumpkins"]
+
+Question 5.
+  def color_valid(color)
+    color == "blue" || color == "green"
+  end
+
+=end
